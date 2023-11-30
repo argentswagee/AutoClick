ea3810867ad1800bdc536f840219e3142a128544
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2023-11-30 15:24:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var fld = document.getElementById('fldroite');
 
 fld.addEventListener('click', function(){
-document.getElementById('f3').style.backgroundColor = '#4f6d7acb';
-document.getElementById('f2').style.backgroundColor = '#ff7029c5';
-document.getElementById('f1').style.backgroundColor = '#ffc0a0c7';
-document.getElementById('body1').style.backgroundColor = '#A7D3A6';
-document.getElementById('pr').style.backgroundColor = '#386C0B';
+document.getElementById('f3').style.backgroundColor = '#9A8F97';
+document.getElementById('f2').style.backgroundColor = '#C3BABA';
+document.getElementById('f1').style.backgroundColor = '#E9E3E6';
+document.getElementById('body1').style.backgroundColor = '#B2B2B2';
+document.getElementById('pr').style.backgroundColor = '#736F72';
 document.getElementById('pr1').style.backgroundColor = '#55a70d';
 document.getElementById('pr2').style.backgroundColor = '#55a70d';
 })
